5ec1216c948069408bbd3eff2d1e802e90f2da61
Shriram <18211a04l2@bvrit.ac.in>
2019-06-03 12:06:05 +0530

    added movement and fixed bg colour glitch

--- 2048/main.cpp
+++ 2048/main.cpp
@@ -4,63 +4,159 @@
 #include<time.h>
 #include<string>
 #include<iostream>
+#include<numeric>
+#include <sstream>
+
+namespace patch
+{
+	template < typename T > std::string to_string(const T& n)
+	{
+		std::ostringstream stm;
+		stm << n;
+		return stm.str();
+	}
+}
+
 SDL_Color white = { 255,255,255,255 };
 class Tile {
 public:
 	int val;
 	Tile() { if ((rand() % 11) > 8)val = 4; else val = 2; }
 	Tile(int val) :val(val) {}
-	const int GetVal() const{ return val; }
+	const int GetVal() const { return val; }
 };
 class Board {
 public:
-	TTF_Font* font = TTF_OpenFont("Roboto-Light.ttf", 32);
+	TTF_Font* font = TTF_OpenFont("Montserrat-Regular.ttf", 32);
 	Tile* BoardMat[4][4];
 	Board() {
 		for (int i = 0; i < 4; i++)
 			for (int j = 0; j < 4; j++)
-				BoardMat[i][j] = nullptr;
+				BoardMat[i][j] = NULL;
 		int row = rand() % 4;
 		int col = rand() % 4;
 		BoardMat[row][col] = new Tile();
 	}
-	void draw(SDL_Renderer* renderer) {
-		
+	void draw(SDL_Renderer * renderer) {
+
 		for (int i = 0; i < 4; i++)
 			for (int j = 0; j < 4; j++)
 			{
-				SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
-				SDL_Rect r= {100*i,100*j,100,100};
-				if (BoardMat[i][j] != nullptr)
+				SDL_Rect r = { 100 * i,100 * j,100,100 };
+				if (BoardMat[i][j] != NULL)
 				{
 					int value = BoardMat[i][j]->GetVal();
 					SDL_SetRenderDrawColor(renderer, (10 * value) % 256, (20 * value) % 256, (30 * value) % 256, 255);
-					SDL_Surface* surface = nullptr;
-					SDL_Texture* texture = nullptr;
-					surface = TTF_RenderText_Solid(font,std::to_string(value).c_str(),white);
+					SDL_Surface* surface = NULL;
+					SDL_Texture* texture = NULL;
+					surface = TTF_RenderText_Solid(font, patch::to_string(value).c_str(), white);
 					texture = SDL_CreateTextureFromSurface(renderer, surface);
 					int recw, rech;
-					SDL_QueryTexture(texture,NULL,NULL,&recw,&rech);
-					SDL_Rect dstrect = { 100*i+29,100*j+29,recw,rech};
+					SDL_QueryTexture(texture, NULL, NULL, &recw, &rech);
+					SDL_Rect dstrect = { 100 * i + 29,100 * j + 29,recw,rech };
 					SDL_RenderFillRect(renderer, &r);
+					SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 					SDL_RenderCopy(renderer, texture, NULL, &dstrect);
 					SDL_FreeSurface(surface);
 					SDL_DestroyTexture(texture);
 				}
+				else SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 			}
 	}
-	void moveright() 
+	void moveright()
 	{
-		for(int i=0;i<4;i++)
+		for (int i = 0; i < 4; i++)
 			for (int j = 0; j < 4; j++)
-			{	
-				if (BoardMat[i][j] != nullptr)
+			{
+				if (BoardMat[i][j] != NULL)
 				{
 					if (i < 3) {
-						if (BoardMat[i+1][j] == nullptr)
-						BoardMat[i+1][j] = BoardMat[i][j];
-						BoardMat[i][j] = nullptr;
+						if (BoardMat[i + 1][j] == NULL) {
+							BoardMat[i + 1][j] = BoardMat[i][j];
+							BoardMat[i][j] = NULL;
+						}
+						else if (BoardMat[i + 1][j]->val == BoardMat[i][j]->val) {
+							BoardMat[i + 1][j]->val = 2 * (BoardMat[i + 1][j]->val);
+							BoardMat[i][j] = NULL;
+						}
+
 					}
+
+					else continue;
+				}
+
+			}
+		spawn();
+	}
+	void moveleft()
+	{
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+			{
+				if (BoardMat[i][j] != NULL)
+				{
+					if (i > 0) {
+						if (BoardMat[i - 1][j] == NULL) {
+							BoardMat[i - 1][j] = BoardMat[i][j];
+							BoardMat[i][j] = NULL;
+						}
+						else if (BoardMat[i - 1][j]->val == BoardMat[i][j]->val) {
+							BoardMat[i - 1][j]->val = 2 * (BoardMat[i - 1][j]->val);
+							BoardMat[i][j] = NULL;
+						}
+
+					}
+
+					else continue;
+				}
+
+			}
+		spawn();
+	}
+	void moveup()
+	{
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+			{
+				if (BoardMat[i][j] != NULL)
+				{
+					if (j > 0) {
+						if (BoardMat[i][j - 1] == NULL) {
+							BoardMat[i][j - 1] = BoardMat[i][j];
+							BoardMat[i][j] = NULL;
+						}
+						else if (BoardMat[i][j - 1]->val == BoardMat[i][j]->val) {
+							BoardMat[i][j - 1]->val = 2 * (BoardMat[i][j - 1]->val);
+							BoardMat[i][j] = NULL;
+						}
+
+					}
+
+					else continue;
+				}
+
+			}
+		spawn();
+	}
+	void movedown()
+	{
+		for (int i = 0; i < 4; i++)
+			for (int j = 0; j < 4; j++)
+			{
+				if (BoardMat[i][j] != NULL)
+				{
+					if (j < 3) {
+						if (BoardMat[i][j + 1] == NULL) {
+							BoardMat[i][j + 1] = BoardMat[i][j];
+							BoardMat[i][j] = NULL;
+						}
+						else if (BoardMat[i][j + 1]->val == BoardMat[i][j]->val) {
+							BoardMat[i][j + 1]->val = 2 * (BoardMat[i][j + 1]->val);
+							BoardMat[i][j] = NULL;
+						}
+
+					}
+
 					else continue;
 				}
 
@@ -71,7 +167,7 @@ public:
 		while (1) {
 			int row = rand() % 4;
 			int col = rand() % 4;
-			if(BoardMat[row][col]==nullptr) { BoardMat[row][col] = new Tile();break; }
+			if (BoardMat[row][col] == NULL) { BoardMat[row][col] = new Tile(); break; }
 		}
 	}
 	~Board() {
@@ -80,12 +176,12 @@ public:
 				delete BoardMat[i][j];
 			}
 		}
-	
+
 	}
 };
 int main(int* argc, char** argv)
 {
-	srand(unsigned int(time(NULL)));
+	//srand(unsigned int(time(NULL)));
 	SDL_Init(SDL_INIT_EVERYTHING);
 	TTF_Init();
 	SDL_Window* window = SDL_CreateWindow("2048", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, SDL_WINDOW_SHOWN);
@@ -93,19 +189,19 @@ int main(int* argc, char** argv)
 	SDL_Event event;
 	bool isRunning = true;
 	Board board;
-	SDL_SetRenderDrawColor(renderer,200,200,200,255);
+	SDL_SetRenderDrawColor(renderer, 200, 200, 200, 255);
 	while (isRunning) {
 		while (SDL_PollEvent(&event))
 		{
 			if (event.type == SDL_QUIT)isRunning = false;
-			if (event.type=SDL_KEYDOWN)
+			if (event.type = SDL_KEYDOWN)
 			{
 				switch (event.key.keysym.sym) {
 				case SDLK_ESCAPE:isRunning = false; break;
-				case SDLK_RIGHT:board.moveright(); break;
-				case SDLK_LEFT:board.moveright(); break;
-				case SDLK_UP:board.moveright(); break;
-				case SDLK_DOWN:board.moveright(); break;
+				case SDLK_RIGHT:board.moveright(); std::cout << "press"; break;
+				case SDLK_LEFT:board.moveleft(); break;
+				case SDLK_UP:board.moveup(); break;
+				case SDLK_DOWN:board.movedown(); break;
 				}
 			}
 			if (event.type = SDL_KEYUP)
@@ -118,7 +214,7 @@ int main(int* argc, char** argv)
 		SDL_RenderClear(renderer);
 		board.draw(renderer);
 		SDL_RenderPresent(renderer);
-	
+
 	}
 	SDL_DestroyRenderer(renderer);
 	SDL_DestroyWindow(window);
